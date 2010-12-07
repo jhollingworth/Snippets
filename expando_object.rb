71a80388e9ec081d2596b0935430ef389e5429e2
James Hollingworth <Jamiehollingworth@gmail.com>
2010-12-07 10:51:32 +0000

    adding expando object

---
+++ expando_object.rb
@@ -0,0 +1,28 @@
+class Context
+	private 
+	
+	def create_method( name, &block )
+		self.class.send( :define_method, name, &block )
+    end
+
+    def create_attr( name )
+        create_method( "#{name}=".to_sym ) { |val| 
+            instance_variable_set( "@" + name, val)
+        }
+
+        create_method( name.to_sym ) { 
+            instance_variable_get( "@" + name ) 
+        }
+    end
+	
+	def method_missing(m, *args, &block)
+		m = m.to_s.sub '=', ''
+		create_attr(m)
+		instance_variable_set "@#{m}", args[0]
+	end
+end 
+
+
+c = Context.new
+
+c.foo = "Bar"
